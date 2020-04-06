5790d4bee5da9b4d54aeee475922e7c66def1c9b
Nicolás Arias <ariasmec06@gmail.com>
2020-04-06 11:14:17 -0300

    ??

---
+++ TP2_1_1.cpp
@@ -0,0 +1,37 @@
+#include<stdio.h>
+#include<ctype.h>
+#include<time.h>
+#include<stdlib.h>
+
+#define N 4
+#define M 5
+
+int main(){
+    int f, c;
+    double mt [N] [M];
+    srand(time(NULL));
+    //________________________________
+    //Aqui se geneta la matriz
+    for (f=0; f < N; f++)
+    {
+        for (c = 0; c < M; c++)
+        {
+            mt [f] [c]= 10 + rand() % (100-10);
+        }       
+    }
+    //_________________________________________
+    //Aqui se muestra la matriz.
+        for (f=0; f < N; f++)
+    {
+        for (c = 0; c < M; c++)
+        {    
+            printf("| %.2lf ", mt [f] [c]);
+        }       
+        printf("|\n");
+    }
+   
+  
+    scanf(" %d");
+
+    return 0;
+}
